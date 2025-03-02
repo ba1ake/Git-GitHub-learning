78ba7bca8a2fabc459a278b9b5212684e24f84f6
ba1ake <blakegabb@gmail.com>
2025-03-03 08:49:46 +1300

    added error control

--- Main.py
+++ Main.py
@@ -6,17 +6,31 @@ name = ""
 ammount = 0
 
 
-
+value_error = False
 
 
 
 
 
  ### main loop ###
-while True:
-    name = input("\nEnter the name of the coin: ")
-    price = input("\nEnter the price of the coin: ")
-    ammount = input("\nEnter the ammount of the coin: ")
-    coins.append(crypto.Crypto(name, price, ammount))
-    if input("Do you want to add another coin? (y/n): ") == "n":
-        break
+
+
+def coin_holdings():
+    while True:
+        name = input("\nEnter the name of the coin: ")
+        try:
+            price = int(input("\nEnter the price of the coin: "))
+            ammount = input("\nEnter the ammount of the coin: ")
+        except ValueError:
+            print("Please enter a number \n")
+            value_error = True
+        if input("Do you want to save?, (y/n): ") == "n" and value_error == False:
+            break
+        if value_error == False:
+            coins.append(crypto.Crypto(name, price, ammount))
+        if input("Do you want to add another coin? (y/n): ") == "n":
+            break
+
+
+
+
