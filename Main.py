1ad67c921e925932495f77ba812976e294a8bbc1
ba1ake <blakegabb@gmail.com>
2025-03-03 11:13:57 +1300

    added loop

--- Main.py
+++ Main.py
@@ -12,11 +12,11 @@ coins = []
 what_to_do = input("What do you want to do? View holdings (1) or add more coins (2): ")
 
 
-
-if what_to_do == "1":
-    crypto.retreive_holdings(coins)
-elif what_to_do == "2":
-    coins = crypto.coin_holdings()
+while True:
+    if what_to_do == "1":
+        crypto.retreive_holdings(coins)
+    elif what_to_do == "2":
+        coins = crypto.coin_holdings()
 
 
 
